e27c3e228f0a4efd2d99a803e5ff49deea41fc8b
MrSurajPawar <surajspawar007@gmail.com>
2024-03-14 19:56:49 +0530

    new add on

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
-import Player from "./components/PLayer.jsx";   //This is the bug of vs code where PLayer is same as Player component
+import Player from "./components/PLayer.jsx"; //This is the bug of vs code where PLayer is same as Player component
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { useState } from "react";
 
 function App() {
-  const [gameTurns , setGameTurns] = useState([]);
-  const [activePlayer , setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
-    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
-    setGameTurns();
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [
+        { sqaure: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
-  
+
   return (
     <main>
       <div id="game-container">
         {/*Players section*/}
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
         {/*Actual board section*/}
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       {/*Logs*/}
       <Log />
