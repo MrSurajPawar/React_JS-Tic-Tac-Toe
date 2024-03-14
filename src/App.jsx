c52f130ccc2d7b0206c58e0ec27e54a669fb8fa0
MrSurajPawar <surajspawar007@gmail.com>
2024-03-14 19:03:05 +0530

    new add on

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import Player from "./components/Player.jsx";
+import Player from "./components/PLayer.jsx";   //This is the bug of vs code where PLayer is same as Player component
+import GameBoard from "./components/GameBoard.jsx";
+import { useState } from "react";
 function App() {
+  const [activePlayer , setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+  }
+  
   return (
     <main>
       <div id="game-container">
         {/*Players section*/}
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
         {/*Actual board section*/}
-        GAME-BOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
       {/*Logs*/}
       LOGS
